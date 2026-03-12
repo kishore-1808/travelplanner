7a289a9adc36e0a3b3bdb46704f3274cd0daa243
Kishore <kishoreselvaraj88@gmail.com>
2026-03-12 20:59:21 +0530

    Hardcode MongoDB URI to fix Vercel Serverless crashes

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,8 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI || "mongodb://ramesh:hQXuK4xMnOqeySBT@ac-ijh7ggh-shard-00-00.xykaksu.mongodb.net:27017,ac-ijh7ggh-shard-00-01.xykaksu.mongodb.net:27017,ac-ijh7ggh-shard-00-02.xykaksu.mongodb.net:27017/?ssl=true&authSource=admin&retryWrites=true&w=majority";
+    const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
       family: 4 // Force IPv4 to avoid localhost IPv6 issues with mongod
     });
